e0c08140defb4bfcec617e0f4b716689eed9f8a6
Paul Steinke <96238837+paulstars@users.noreply.github.com>
2023-06-08 17:06:41 +0200

    Hinzufügen einer class NamesList mit newName, getAllNames, removeName und clearNames

---
+++ Data/NamesList.cs
@@ -0,0 +1,49 @@
+
+namespace Data
+{
+    /// <summary>
+    /// A List including the Names of all active Users.
+    /// </summary>
+    public class NamesList
+    {
+        List<string> names = new List<string>();
+        
+        public void newName(string name){
+            if (names.Contains(name)){
+                throw new Exception("${name} wird bereits von jemand anderem benutzt!") ;
+            }else
+            {
+                names.Add(name);
+            }
+        }
+
+        public string getAllNames(){
+            string allNames = "";
+            for (int i = 0; i < names.Count; i++)
+            {
+                allNames = allNames + ", " + names[i];
+            }
+            return allNames;
+        }
+
+        public void removeName(string name){
+            if (!names.Contains(name))
+            {
+                throw new Exception("${name} wird nicht benutzt!") ;
+            }else
+            {
+                names.Remove(name);
+            }
+        }
+
+        public void clearNames(){
+            if (names.Count == 0)
+            {
+                throw new Exception("Es sind keine Namen vorhanden!") ;
+            }else
+            {
+                names.Clear();
+            }
+        }
+    }
+}
