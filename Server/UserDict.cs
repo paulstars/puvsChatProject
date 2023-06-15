b47be1a1fc970cb93220f00db429410586c6f038
Paul Steinke <96238837+paulstars@users.noreply.github.com>
2023-06-15 18:05:59 +0200

    Weitere Änderungen

--- Server/UserDict.cs
+++ Server/UserDict.cs
@@ -10,21 +10,43 @@ namespace Server
     {
         private Dictionary<string, string> users = new Dictionary<string, string>();
 
-        public bool DoesUserExist(string user)
+        public bool User(string key, string user)
         {
-            return this.users.ContainsValue(user);
+            if (this.users.ContainsValue(user))
+            {
+                if (this.users.ContainsKey(key))
+                {
+                    this.users.TryGetValue(key, out string? name);
+                    if (name == user)
+                    {
+                        Console.WriteLine($"Client '{user}' verified!");
+                        return true;
+                    }
+                    Console.WriteLine($"Client '{user}' already registered!");
+                    return false;
+                }
+            }
+            if (this.users.ContainsKey(key))
+            {
+                
+            }
+            if (key == "-1")
+            {
+                return NewUser(user);
+            }
         }
 
-        public bool NewUser(string key, string user){
-            if (!this.users.ContainsKey(key) && !this.users.ContainsValue(user))
+        public bool NewUser(string user){ 
+            var key = Convert.ToString(Guid.NewGuid());
+            
+            if (!this.users.ContainsValue(user))
             {
                 this.users.TryAdd(key, user);
                 Console.WriteLine($"Client '{user}' registered!");
                 return true;
             }
             
-            Console.WriteLine($"Client '{user}' already registered!");
-            return false;
+            
         }
 
         public string GetAllUser()
