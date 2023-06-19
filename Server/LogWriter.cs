788ce61d4be838276627c879e1199293fe29df85
Paul Steinke <96238837+paulstars@users.noreply.github.com>
2023-06-19 12:42:47 +0200

    New LogWriter.cs + Log.txt

---
+++ Server/LogWriter.cs
@@ -0,0 +1,35 @@
+﻿
+
+namespace Server;
+
+/// <summary>
+/// Writes lines into log.txt or clears the log-file
+/// </summary>
+public class LogWriter
+{
+    private static readonly string FilePath = Path.Combine(Directory.GetCurrentDirectory(), "Log.txt");
+
+    private readonly object log = new();
+    
+    /// <summary>
+    /// Writes the given line into the log-file and the console
+    /// </summary>
+    /// <param name="line">string - the content of this log message</param>
+    public void WriteLogLine(string line)
+    {
+        lock (this.log)
+        {
+            using var writer = new StreamWriter(FilePath, true);
+            writer.WriteLineAsync(line);
+            Console.WriteLine(line);
+        }
+    }
+
+    /// <summary>
+    /// Empties log.txt
+    /// </summary>
+    public async void ClearLogFile()
+    {
+        await File.WriteAllTextAsync(FilePath, string.Empty);
+    }
+}
